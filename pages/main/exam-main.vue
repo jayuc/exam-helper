71c8823289802f2f85b20b7e896e81dba53f0700
余杰 <402842327@qq.com>
2020-05-22 15:31:10 +0800

    答题主界面翻页

--- pages/main/exam-main.vue
+++ pages/main/exam-main.vue
@@ -4,7 +4,7 @@
 			{{exerciseType}}（{{sectionName}}）
 			<view class="abs">{{questionIndex}}/{{questionTotal}}</view>
 		</view>
-		<view class="exam-main-container" @touchstart="start" @touchend="end">
+		<view class="exam-main-container" :style="containerStyle" @touchstart="start" @touchend="end">
 			<view class="h10"></view>
 			<view class="question">
 				<text class="qtColor">（{{questionType}}）</text> {{questionContent}}?
@@ -25,24 +25,40 @@
 				subjectName: 'Java',
 				sectionName: 'Java基础',
 				questionType: '简单题',
-				questionContent: '面向对象和面向过程的区别面向对象和面向过程的区别面向对象和面向过程的区别面向对象和面向过程的区别面向对象和面向过程的区别',
+				questionContent: '面向对象和面向过程的区别',
 				anwserContent: '这是一个简单的问题',
 				questionIndex: 1,
-				questionTotal: 15
+				questionTotal: 15,
+				startData: {},
+				containerHeight: 300
+			}
+		},
+		computed: {
+			containerStyle(){
+				return "height:" + this.containerHeight + "px";
 			}
 		},
 		onLoad() {
+			
+		},
+		mounted() {
+			let that = this;
+			uni.getSystemInfo({     // 获取屏幕高度
+				success(systemInfo) {
+					let head = uni.createSelectorQuery().select('.exam-main-head');
+					head.boundingClientRect(function(data){  // 获取头部元素高度
+						that.containerHeight = systemInfo.windowHeight - data.height - 1;
+					}).exec();
+				}
+			})
 		},
 		methods: {
 			start(e){
-			                    
-			    this.startData.clientX=e.changedTouches[0].clientX;
-			                 
-			    this.startData.clientY=e.changedTouches[0].clientY;
+			    this.startData.clientX = e.changedTouches[0].clientX;
+			    this.startData.clientY = e.changedTouches[0].clientY;
 			},
 			end(e){
-			    // console.log(e)
-			    const subX=e.changedTouches[0].clientX-this.startData.clientX;
+			    const subX=e.changedTouches[0].clientX - this.startData.clientX;
 			    const subY=e.changedTouches[0].clientY - this.startData.clientY;
 			    if(subY>50 || subY<-50){
 			        console.log('上下滑')
@@ -75,6 +91,7 @@
 		padding: 0rpx 25rpx;
 		background-color: #FFFFFF;
 		font-size: 28rpx;
+		height: 82rpx;
 		line-height: 82rpx;
 		border-bottom: 1px solid #f3f3f3;
 	}
