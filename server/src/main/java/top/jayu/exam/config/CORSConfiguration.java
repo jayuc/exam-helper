4536ee2b67949500a8d9cadbf268e38eb206913e
余杰 <402842327@qq.com>
2020-05-28 14:39:08 +0800

    添加跨域问题

--- server/src/main/java/top/jayu/exam/config/CORSConfiguration.java
+++ server/src/main/java/top/jayu/exam/config/CORSConfiguration.java
@@ -2,8 +2,10 @@ package top.jayu.exam.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 @Configuration
@@ -11,6 +13,13 @@ public class CORSConfiguration extends WebMvcConfigurationSupport {
 
     @Autowired
     SecurityHandler securityHandler;
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry){
+        registry.addViewController( "/" ).setViewName( "forward:/index.html" );
+        registry.setOrder( Ordered.HIGHEST_PRECEDENCE );
+        super.addViewControllers( registry );
+    }
 
     @Override
     protected void addCorsMappings(CorsRegistry registry) {
